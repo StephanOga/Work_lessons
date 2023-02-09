a1c3412e43dfd88c38efa5070840755780802cd5
Stepan <sud_pravo@bk.ru>
2023-02-09 21:36:23 +0700

    обавлен новый поекt exempleр006 Привет Элиночка

---
+++ Exemples/exemple006/Program.cs
@@ -0,0 +1,13 @@
+﻿ Console.Write("Введите имя пользователя ");
+ string username = Console.ReadLine();
+
+ if(username.ToLower() == "элина")
+ {
+    Console.WriteLine("Ура этоже ЭЛИНА!!! ");
+ }
+ else 
+ {
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+ }
+ 
